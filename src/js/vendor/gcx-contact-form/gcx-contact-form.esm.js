afbbb293a93609139e7e8f48b3bd6e01c75d0633
Edwin Ilovares <edwini@studio31.io>
2022-06-27 23:42:15 -0600

    gcx-full and gcx-landing StencilJS components

---
+++ src/js/vendor/gcx-contact-form/gcx-contact-form.esm.js
@@ -0,0 +1,1 @@
+import{p as e,b as t}from"./p-44dfe3e3.js";(()=>{const t=import.meta.url,r={};return""!==t&&(r.resourcesUrl=new URL(".",t).href),e(r)})().then((e=>t([["p-95e50b12",[[0,"feedback-form",{article:[1],rate:[32],name:[32],email:[32],suggestions:[32],formState:[32]},[[0,"rateArticle","todoCompletedHandler"]]],[1,"feedback-stars",{rating:[2]}]]]],e)));
