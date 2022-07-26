f6fec7dabad6c08ded35324666a06f4da51f46e1
Edwin Ilovares <edwindh@gmail.com>
2022-07-25 19:38:54 -0600

    lint fixes

--- src/helpers/schema.js
+++ src/helpers/schema.js
@@ -1,6 +1,6 @@
 'use strict'
 
 module.exports = (uiRoot) => {
-    const dots = uiRoot.split('../').length - 2;
-    return ".".repeat(dots) + "/_attachments/schema.json".trimStart()
+  const dots = uiRoot.split('../').length - 2
+  return '.'.repeat(dots) + '/_attachments/schema.json'.trimStart()
 }
--- src/js/08-gcx-helios.js
+++ src/js/08-gcx-helios.js
@@ -1,71 +1,67 @@
 ;
-(function() {
-    'use strict';
-    console.log("cargando JS 08-gcx-helios");
-    window.intercomSettings = { app_id: "bpbxnnmr" };
+(function () {
+  'use strict'
+  console.log('cargando JS 08-gcx-helios')
+  window.intercomSettings = { app_id: 'bpbxnnmr' }
 
-    var w = window;
-    var ic = w.Intercom;
-    if (typeof ic === "function") {
-        ic('reattach_activator');
-        ic('update', w.intercomSettings);
+  var w = window
+  var ic = w.Intercom
+  if (typeof ic === 'function') {
+    ic('reattach_activator')
+    ic('update', w.intercomSettings)
+  } else {
+    var d = document
+    var i = function () {
+      i.c(arguments)
+    }
+    i.q = []
+    i.c = function (args) {
+      i.q.push(args)
+    }
+    w.Intercom = i
+    var l = function () {
+      var s = d.createElement('script')
+      s.type = 'text/javascript'
+      s.async = true
+      s.src = 'https://widget.intercom.io/widget/bpbxnnmr'
+      var x = d.getElementsByTagName('script')[0]
+      x.parentNode.insertBefore(s, x)
+    }
+    if (w.attachEvent) {
+      w.attachEvent('onload', l)
     } else {
-        var d = document;
-        var i = function() {
-            i.c(arguments);
-        };
-        i.q = [];
-        i.c = function(args) {
-            i.q.push(args);
-        };
-        w.Intercom = i;
-        var l = function() {
-            var s = d.createElement('script');
-            s.type = 'text/javascript';
-            s.async = true;
-            s.src = 'https://widget.intercom.io/widget/bpbxnnmr';
-            var x = d.getElementsByTagName('script')[0];
-            x.parentNode.insertBefore(s, x);
-        };
-        if (w.attachEvent) {
-            w.attachEvent('onload', l);
-        } else {
-            w.addEventListener('load', l, false);
-        }
+      w.addEventListener('load', l, false)
+    }
+  }
+
+  /*Dark theme verification*/
+  var theme = window.localStorage.getItem('theme')
+  window.addEventListener('DOMContentLoaded', (event) => {
+    var checkbox = document.getElementById('switch')
+    var root = document.getElementsByTagName('html')[0]
+    if (theme) {
+      root.classList.add('dark-mode')
+      checkbox.checked = true
+    } else {
+      checkbox.checked = false
     }
 
-    /*Dark theme verification*/
-    var theme = window.localStorage.getItem('theme');
-    window.addEventListener('DOMContentLoaded', (event) => {
-        var checkbox = document.getElementById('switch');
-        var root = document.getElementsByTagName('html')[0];
-        if (theme) {
-            root.classList.add('dark-mode');
-            checkbox.checked = true;
+    document.addEventListener('click', function (event) {
+      if (event.target.matches('.switch')) {
+        var root = document.getElementsByTagName('html')[0]
+        if (event.target.checked) {
+          window.localStorage.setItem('theme', 'dark')
+          root.classList.add('dark-mode')
         } else {
-            checkbox.checked = false;
+          window.localStorage.removeItem('theme')
+          root.classList.remove('dark-mode')
         }
-
-        document.addEventListener('click', function(event) {
-            if (event.target.matches('.switch')) {
-                var root = document.getElementsByTagName('html')[0]
-                if (event.target.checked) {
-                    window.localStorage.setItem('theme', 'dark');
-                    root.classList.add('dark-mode');
-                } else {
-                    window.localStorage.removeItem('theme');
-                    root.classList.remove('dark-mode');
-                }
-
-            }
-        }, false);
-    });
-
-
+      }
+    }, false)
+  })
 })()
 
-
-document.querySelector('.nav-item.toggler').addEventListener('click', function(event) {
-    if (!event.target.matches('li[data-depth="1"]') && !event.target.matches('.switch')) return;
-    if (event.target.matches('li[data-depth="1"]')) { event.target.classList.toggle('is-active'); }
-});
+document.querySelector('.nav-item.toggler').addEventListener('click', function (event) {
+  if (!event.target.matches('li[data-depth="1"]') && !event.target.matches('.switch')) return
+  if (event.target.matches('li[data-depth="1"]')) { event.target.classList.toggle('is-active') }
+})
