d506b31cbebf876ffdd6f0ed8472f81d69e6679f
Edwin Ilovares <edwindh@gmail.com>
2022-08-12 16:50:45 -0600

    Fixes css and sidebar for landing page

--- src/js/08-gcx-helios.js
+++ src/js/08-gcx-helios.js
@@ -1,6 +1,12 @@
 /*Dark theme verification*/
 var theme = window.localStorage.getItem('theme')
 window.addEventListener('DOMContentLoaded', (event) => {
+  var path = window.location.pathname
+  var page = path.split('/').pop()
+  var name = page.replace('.html', '')
+  console.log(name)
+  const sidebar = document.querySelector('.sidebar')
+  const col = document.getElementById('collapse')
   var checkbox = document.getElementById('switch')
   var root = document.getElementsByTagName('html')[0]
   if (theme) {
@@ -8,6 +14,11 @@ window.addEventListener('DOMContentLoaded', (event) => {
     checkbox.checked = true
   } else {
     checkbox.checked = false
+  }
+
+  if (name === 'landing') {
+    col.checked = true
+    sidebar.classList.add('small-container')
   }
 
   document.addEventListener('click', function (event) {
