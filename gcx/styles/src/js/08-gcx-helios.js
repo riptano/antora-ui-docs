eab096629d418e7f4531e8ef171611554b91e6f0
Kenni Martínez <57879404+KenniM@users.noreply.github.com>
2023-03-28 16:06:18 -0600

    Dark mode cards improvements
    Also, this includes styles for the new Hero blocks

--- gcx/styles/src/js/08-gcx-helios.js
+++ gcx/styles/src/js/08-gcx-helios.js
@@ -135,3 +135,25 @@ document.querySelectorAll(".nav-item> span.nav-text").forEach(function (item) {
     item.parentElement.classList.toggle("is-active");
   })
 })
+
+var heroBlock = document.querySelector('.dsHeroBlock')
+var heroTitle = document.querySelector('.paragraph.hero.title p').innerHTML
+var heroContent = document.querySelector('.paragraph.hero.content p').innerHTML
+
+var heroHTML = `<div class="dsHeroContent">
+  <div class="dsHeroTitle">
+    <h1> ${heroTitle} </h1>
+  </div>
+  <div class="dsHeroDescription">
+    <p> ${heroContent} </p>
+  </div>
+</div>`
+
+document.querySelector('.dsHeroBlock').remove()
+
+if (heroBlock) {
+  var target = document.querySelector('main.article')
+  target.insertBefore(heroBlock, target.children[1])
+
+  document.querySelector('.dsHeroBlock').innerHTML = heroHTML
+}
